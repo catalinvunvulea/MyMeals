8e1615d1a1afe3582fe84d458c295bc76c8d0868
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-06-24 17:03:48 +0100

    Styling and Theming

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -10,18 +10,22 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(5),
-      child: Text(title),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [ //grtadient starting the firs color to the second
-            color.withOpacity(0.7), //first color
-            color, //second color
-          ],
-          begin: Alignment.topLeft, //gradient will begin from topLeft
-          end: Alignment.bottomRight, // and will end to bottomRight
-        ),
-        borderRadius: BorderRadius.circular(15)
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
       ),
+      decoration: BoxDecoration(
+        boxShadow: BoxShadow.lerp(1, 1, 1),
+          gradient: LinearGradient(
+            colors: [
+              //grtadient starting the firs color to the second
+              color.withOpacity(0.3), //first color
+              color, //second color
+            ],
+            begin: Alignment.topLeft, //gradient will begin from topLeft
+            end: Alignment.bottomRight, // and will end to bottomRight
+          ),
+          borderRadius: BorderRadius.circular(15)),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Meals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ), 
+        primarySwatch: Colors.purple,
+        accentColor: Colors.pink,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
       home: CategoriesScreen(),
     );
   }
