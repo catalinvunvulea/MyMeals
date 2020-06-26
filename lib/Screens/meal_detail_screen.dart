0e75be43c65e8b13ce8b653dd90138bba38fe5bb
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-06-26 11:05:35 +0100

    Finish the Meal Detail Page
    replace widget with func, SingleChildScrollView, istView.builder,ListTile

--- lib/Screens/meal_detail_screen.dart
+++ lib/Screens/meal_detail_screen.dart
@@ -1,15 +1,95 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class MealDetailScreen extends StatelessWidget {
   static const routeName = './meal-detail-screen';
 
+  Widget buildSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget buildContainer(
+      BuildContext context, double height, double width, Widget widget) {
+    return Container(
+        decoration: BoxDecoration(
+          color: Colors.yellowAccent[50],
+          border: Border.all(
+            color: Colors.grey,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        height: MediaQuery.of(context).size.height * height,
+        width: MediaQuery.of(context).size.width * width,
+        child: widget);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final mealId = ModalRoute.of(context).settings.arguments as String;
+    final mediaQuery = MediaQuery.of(context).size;
+    final mealId = ModalRoute.of(context).settings.arguments;
+    final selectedMeal = DUMMY_MEALS.firstWhere((element) =>
+        element.id ==
+        mealId); //if will go through the Dummy meals and it will pick the data when the func is true (element.id == mealId)
     return Scaffold(
-      appBar: AppBar(title: Text('title $mealId'),),
-          body: Center(
-        child: Text('The meal - $mealId'),
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: mediaQuery.height * 0.3,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            buildSectionTitle(context, "Ingredients"),
+            buildContainer(
+              context,
+              0.22,
+              0.8,
+              ListView.builder(
+                itemBuilder: (ctx, index) => Card(
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+                    child: Text(
+                      '${selectedMeal.ingredients[index]}',
+                    ),
+                  ),
+                ),
+                itemCount: selectedMeal.ingredients.length,
+              ),
+            ),
+            buildSectionTitle(context, 'Steps'),
+            buildContainer(
+                context,
+                0.22,
+                0.8,
+                ListView.builder(
+                    itemBuilder: (ctx, index) => Column(
+                          children: <Widget>[
+                            ListTile(
+                              leading: CircleAvatar(
+                                child: Text('# ${index + 1}'),
+                              ),
+                              title: Text('${selectedMeal.steps[index]}'),
+                            ),
+                            Divider(), //add a single line
+                          ],
+                        ),
+                    itemCount: selectedMeal.steps.length))
+          ],
+        ),
       ),
     );
   }
